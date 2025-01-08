9efe78803c70fb1ab02b4da674363fadbd21df61
vagalemj <mjv.vagale@gmail.com>
2025-01-08 18:18:10 +0530

    initial commit

---
+++ Strings/StringMethods.py
@@ -0,0 +1,11 @@
+# Strings are immutable in python
+# Ways to create a string in python
+
+s1 = 'My name is Madhu'
+s2 = "I'm a software developer"
+s3 = '''I'm a software developer'''
+s4 = """I'm a software developer"""
+print(s1)
+print(s2)
+print(s3)
+print(s4)
